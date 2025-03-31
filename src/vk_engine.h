b11b80bf29be6b1ba1bc2242ec7a91c49c6eee54
Nikhil Maurya <agentnuclear27@gmail.com>
2025-03-31 11:38:02 +0530

    Vulkan Initialization
    Initialized using Vkbootstrap
    - Device
    - Instance
    - Swapchain
    - Surface
    - swapchain images and imageviews

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -12,6 +12,21 @@ public:
 	int _frameNumber {0};
 	bool stop_rendering{ false };
 	VkExtent2D _windowExtent{ 1000 , 650 };
+	bool bUseValidationLayers = true;
+
+	VkInstance _instance; // Vulkan Lib Handle
+	VkDebugUtilsMessengerEXT _debug_messenger; //Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; //GPU chosen as the default device
+	VkDevice _device; //Vulkan device for commands
+	VkSurfaceKHR _surface; //Vulkan Window Surface
+
+	//Vars for swapchain
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchianImages;
+	std::vector<VkImageView> _swapchianImageViews;
+	VkExtent2D _swapchainExtent;
 
 	struct SDL_Window* _window{ nullptr };
 
@@ -28,4 +43,12 @@ public:
 
 	//run main loop
 	void run();
+
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
